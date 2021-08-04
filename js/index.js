b6c713de5fcc091e05f4cd54e58ef8987122d5bd
Dimmies <dimmiestv@gmail.com>
2021-08-04 11:11:50 -0400

    Update 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // http://142.44.143.209:30120/players.json
+
+// http://54.37.245.190:30112/players.json
+// http://54.37.245.190:30112/dynamic.json
+
+    // $.getJSON("https://jsonp.afeld.me/?url=http://54.37.245.190:30112/dynamic.json", function(data){
+    //     console.log("test", data)
+    // })
+
+// fetch("http://54.37.245.190:30112/dynamic.json", {
+//     method: "no-cors",
+// }).then(function(data) {
+//     console.log(data)
+// })
