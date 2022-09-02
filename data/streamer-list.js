f88df9b044ca902df7eafb70f9c49b523e0f5086
Dimmies <dimmiestv@gmail.com>
2022-09-02 14:30:09 -0400

    update

---
+++ data/streamer-list.js
@@ -0,0 +1,62 @@
+let StreamerList = {
+    0: {
+        channel: "dimmiestv",
+        label: "DimmiesTV",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/2be300ac-1654-477f-8e7f-be0c6dab5b98-profile_image-70x70.png",
+    },
+    1: {
+        channel: "footsoldierz",
+        label: "FootSoldierZ",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/e9005f8a-d411-4b71-96e4-62d93b867616-profile_image-150x150.png",
+    },
+    2: {
+        channel: "cloverism",
+        label: "Cloverism",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/722301e4-46cc-4d3f-a824-9abc867f41e3-profile_image-70x70.png",
+    },
+    3: {
+        channel: "chromello",
+        label: "Chromello",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/33cd3c0b-67d2-42b4-b207-d6407d8cfc63-profile_image-70x70.png",
+    },
+    4: {
+        channel: "irvrp",
+        label: "IRVRP",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/6aae5267-868f-4465-aae6-82a3f5ab75c8-profile_image-70x70.png",
+    },
+    5: {
+        channel: "req_nadeframe",
+        label: "Req_NadeFrame",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/a5eaf045-4b32-455d-9a9a-79bfaf688928-profile_image-70x70.png",
+    },
+    6: {
+        channel: "safefield",
+        label: "Safefield",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/0ad37eb8-8257-46e7-b484-1b328278ab8b-profile_image-70x70.png",
+    },
+    7: {
+        channel: "notoriousbuddhaa",
+        label: "NotoriousBuddhaa",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/ab6f6834-7380-4621-9460-600fa6139d64-profile_image-70x70.png",
+    },
+    8: {
+        channel: "noncanadianbear",
+        label: "Noncanadianbear",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/3a4ce028-e20b-4bec-adf5-2ddf8216ce02-profile_image-70x70.png",
+    },
+    9: {
+        channel: "jasaratplaying",
+        label: "Jasaratplaying",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/88c76085-9146-42fc-ba22-0a030fe0f073-profile_image-70x70.png",
+    },
+    10: {
+        channel: "ghostieboi97",
+        label: "GhostieBoi97",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/ebd55722-449e-4016-90d4-78f6788b4f46-profile_image-70x70.png",
+    },
+    11: {
+        channel: "iherring",
+        label: "iherring",
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/93bfc03c-6bda-42ff-9add-56cfdd34fceb-profile_image-70x70.png",
+    },
+}
